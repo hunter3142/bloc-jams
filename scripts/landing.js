e1a9e7dbecef5a6947876352996178b8cabc5ebb
Mathew <hunter3142@gmail.com>
2017-04-13 23:57:15 -0400

    correct translate in landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,9 +4,9 @@ var animatePoints = function() {
  
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translate(0)";
-         points[index].style.msTransform = "scaleX(1) translate(0)";
-         points[index].webkitTransform = "scaleX(1) translate(0)";
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].webkitTransform = "scaleX(1) translateY(0)";
      } 
     for(var i=0; i < points.length; i++) {
         revealPoint(i);
